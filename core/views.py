ea36c1dbba1dc18b5198b97b101fc03a6c66ae66
issa <ndayishimiyeissa11@gmail.com>
2026-03-11 15:07:33 +0200

    step3

--- core/views.py
+++ core/views.py
@@ -1,10 +1,70 @@
 from django.http import JsonResponse
-from django.shortcuts import render
-from core.models import Testing
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from core.models import Testing, Transaction
+from core.serializers import TestingSerializer, TransactionSerializer
+
 
 def testing_view(request):
-    # For now, return a simple static JSON response
-    return JsonResponse({'message': 'Hello, world!'})
+    data = Testing.objects.all()
+    serializer = TestingSerializer(data, many=True)
+    return JsonResponse(serializer.data, safe=False)
 
-def health_check(request):
-    return JsonResponse({'status': 'ok'})
+
+class TransactionListView(APIView):
+
+    def get(self, request):
+        transactions = Transaction.objects.all()
+        serializer = TransactionSerializer(transactions, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TransactionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TransactionDetailView(APIView):
+
+    def get_object(self, id):
+        try:
+            return Transaction.objects.get(id=id)
+        except Transaction.DoesNotExist:
+            return None
+
+    def get(self, request, id):
+        transaction = self.get_object(id)
+        if transaction is None:
+            return Response(
+                {"error": "Transaction not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        serializer = TransactionSerializer(transaction)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        transaction = self.get_object(id)
+        if transaction is None:
+            return Response(
+                {"error": "Transaction not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        serializer = TransactionSerializer(transaction, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        transaction = self.get_object(id)
+        if transaction is None:
+            return Response(
+                {"error": "Transaction not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        transaction.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
